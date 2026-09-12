676a91afb7c4dce50e2c22120a4d21c21350ada7
Magnolia <magnolia71102@gmail.com>
2026-09-12 17:38:16 +0800

    修改顺序栈的push函数错误

--- 04_stack/19_array_stack_push.cpp
+++ 04_stack/19_array_stack_push.cpp
@@ -3,7 +3,7 @@
 // Task 19: array stack push.
 // TODO: Implement this exercise yourself.
 bool Push(SqStack &S, ElemType e){
-    if((S.top-S.base) == 0) return false;
+    if((S.top-S.base) == S.stacksize) return false;
     *(S.top) = e;
     ++(S.top);
     return true;
